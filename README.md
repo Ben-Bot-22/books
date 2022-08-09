1fab61d267b5a00dc2e02ca700b2f93b5389c8e1
Ben Whitfield <whitfieldbenjamin@gmail.com>
2022-08-09 14:10:19 -0400

    Library implemented

---
+++ README.md
@@ -0,0 +1,46 @@
+# Calculator
+
+This is a solution to the "Library" project in the [Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-library).  The Odin project is a open source tutorial for learning web development.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users adds book cards to a library website.   
+
+### Links
+
+- Solution URL: [Code on GitHub](https://github.com/Ben-Bot-22/books)
+- Live Site URL: [GitHub pages live URL]()
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- JavaScript (vanilla)
+- Visual Studio Code
+
+### What I learned
+
+1) When implementing the modal form to collect user data, I learned that using the CSS visibility property hides buttons underneath.  This was easy to debug with Chrome developer tools.  
+
+2) The checkbox label uses the "for" attribute to match the checkbox "id" attribute.  This allows the user to click the text and affect the checkbox.
+
+3) I used [data attribute](https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes) to identify the DOM element with the array index.  This allowed the user to remove books from the library.  To ensure the array indexes (in the DOM) remained accurate, I reloaded the books every time an element was removed.
+
+## Author
+
+- Twitter - [@benBot22](https://www.twitter.com/benBot22)
+
--- index.js
+++ index.js
@@ -1,14 +1,177 @@
-function theHobbit(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+let myLibrary = [];
+let modalActive = false;
+
+const bookElement = document.querySelector(".book-content");
+const addButton = document.querySelector(".add-btn");
+const bookSection = document.querySelector(".book-content");
+const modalForm = document.querySelector(".modal-content");
+const formBtn = document.getElementById("submit");
+
+addButton.addEventListener("click", toggleModal);
+modalForm.addEventListener("submit", handleSubmit);
+modalForm.addEventListener("formdata", addBookToLibrary);
+
+//Get form data
+const title = document.getElementById("title");
+
+//Event delegation for remove button
+document.addEventListener("click", removeBook);
+
+//Event delegation for read button
+document.addEventListener("click", function (e) {
+  if (e.target.id === "read-btn") {
+    // toggle read
+    const index =
+      e.target.parentElement.parentElement.getAttribute("data-index");
+    // update library data
+    const book = myLibrary[Number(index)];
+    book.toggleRead();
+    // update button
+    setReadButton(book, e.target);
+  }
+});
+
+function toggleModal(e) {
+  if (modalActive) {
+    // turn modal off
+    addButton.innerHTML = "+ add book";
+    bookSection.classList.remove("blur");
+    modalForm.style.animationName = "pop-out";
+    setTimeout(function() {
+      modalForm.style.display = "none";
+    }, 150);
+  } else {
+    // turn modal on
+    addButton.innerHTML = "close";
+    bookSection.classList.add("blur");
+    modalForm.style.display = "flex";
+    modalForm.style.animationName = "pop-in";
+  }
+  modalActive = !modalActive;
 }
 
-theHobbit.prototype.info = function() {
-    return this.title + ' by ' + this.author + ', ' + this.pages.toString() + ' pages'; 
+window.addEventListener('click', event => {
+  if (event.target != addButton && modalActive && !event.target.closest(".modal-content"))
+  {
+    toggleModal();
+  }
+});
+
+// esc key shortcut
+window.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    if (modalActive) toggleModal();
+  }
+});
+
+function Book(title, pages, read) {
+  this.title = title;
+  this.pages = pages;
+  this.read = read;
 }
 
-const book = new theHobbit('The Hobbit', 'JRR Tolkein', 295, false);
+Book.prototype.toggleRead = function () {
+  this.read = !this.read;
+};
 
-console.log(book.info());
+function removeBook(e) {
+  if (e.target.id === "remove-btn") {
+    // get element number
+    const index =
+      e.target.parentElement.parentElement.getAttribute("data-index");
+    // remove from myLibrary
+    myLibrary.splice(Number(index), 1);
+    // remove from DOM
+    while (bookSection.firstChild) {
+      bookSection.removeChild(bookSection.firstChild);
+    }
+    displayBooks();
+    // printLibrary();
+  }
+}
+
+function printLibrary() {
+  myLibrary.forEach(function (book) {
+    console.log(book.title + " " + myLibrary.indexOf(book));
+  });
+}
+
+function addBookToLibrary(e) {
+  const data = e.formData;
+  // get the data
+  let hasRead = false;
+  if (data.get("has-read") === "on") {
+    hasRead = true;
+  }
+  let newBook = new Book(data.get("title"), data.get("pages"), hasRead);
+  myLibrary.push(newBook);
+  renderBook(newBook);
+  toggleModal();
+  modalForm.reset();
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  new FormData(modalForm);
+}
+
+function renderBook(book) {
+  const card = createBookCard(book);
+  bookElement.append(card);
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+    renderBook(book);
+  });
+}
+
+Book.prototype.info = function () {
+  return this.title + this.pages.toString() + " pages";
+};
+
+function setReadButton(book, readBtn) {
+  if (book.read) {
+    readBtn.textContent = "read";
+  } else {
+    readBtn.textContent = "not read";
+  }
+}
+
+const createBookCard = (book) => {
+  const index = myLibrary.indexOf(book);
+  // Create elements
+  const bookCard = document.createElement("div");
+  bookCard.setAttribute("data-index", index.toString()); //access index
+  const content = document.createElement("div");
+  const title = document.createElement("div");
+  const pages = document.createElement("div");
+  const readBtn = document.createElement("button");
+  const removeBtn = document.createElement("button");
+  // Add classes
+  bookCard.classList.add("book-card");
+  content.classList.add("content");
+  readBtn.classList.add("card-btn");
+  readBtn.setAttribute("id", "read-btn");
+  removeBtn.classList.add("card-btn");
+  removeBtn.setAttribute("id", "remove-btn");
+  // Set text content
+  title.textContent = `"${book.title}"`;
+  pages.textContent = `${book.pages} pages`;
+  removeBtn.textContent = "remove";
+  setReadButton(book, readBtn);
+  // Append to DOM
+  bookCard.appendChild(content);
+  content.appendChild(title);
+  content.appendChild(pages);
+  content.appendChild(readBtn);
+  content.appendChild(removeBtn);
+  return bookCard;
+};
+
+// myLibrary.push(new Book("The Hobbit", 295, false));
+// myLibrary.push(new Book("The 3 Body Problem", 295, false));
+// myLibrary.push(new Book("Don't Make Me Think", 150, false));
+displayBooks();
+
+
